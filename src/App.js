776314302f4f2f42ea324363c3336161314f2dd4
Kunegunda <kosekkuengunda@wp.pl>
2023-03-07 20:41:26 +0100

    href

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <HomePage />
       <BrowserRouter>
         <Routes>
           <Route path='/ProjectReact-Router/' element={<HomePage />} />
